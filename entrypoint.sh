4a4cb28d2e90deb2bcfc6db8d6741f562f569b6b
Billie Cleek <bhcleek@gmail.com>
2022-07-02 12:30:27 -0700

    ensure SSH keys have a trailing newline
    Add a trailing newline to SSH keys to conform to SSH expectations and
    prevent an authentication failure due to the lack of a trailing newline.

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,13 +21,13 @@ fi
 
 if [[ -n "${UPSTREAM_SSH_PRIVATE_KEY}" ]]; then
   printf "Saving UPSTREAM_SSH_PRIVATE_KEY\n" >&2
-  printf "%s" "${UPSTREAM_SSH_PRIVATE_KEY}" | sed 's/\\n/\n/g' > "${sshtmp}/src_rsa"
+  printf "%s\n" "${UPSTREAM_SSH_PRIVATE_KEY}" | sed 's/\\n/\n/g' > "${sshtmp}/src_rsa"
   chmod 600 "${sshtmp}/src_rsa"
 fi
 
 if [[ -n "${DESTINATION_SSH_PRIVATE_KEY}" ]]; then
   printf "Saving DESTINATION_SSH_PRIVATE_KEY\n" >&2
-  printf "%s" "${DESTINATION_SSH_PRIVATE_KEY}" | sed 's/\\n/\n/g' > "${sshtmp}/dst_rsa"
+  printf "%s\n" "${DESTINATION_SSH_PRIVATE_KEY}" | sed 's/\\n/\n/g' > "${sshtmp}/dst_rsa"
   chmod 600 "${sshtmp}/dst_rsa"
 fi
 
